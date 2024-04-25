1fdb9bf971c7672e30d8802ccc069bc64ce9dedd
MartinZuniga <martin.zuniga@usach.cl>
2024-04-25 08:38:23 -0400

    integracion con el fron

--- src/main/java/com/martin/ecommerce/springecommerce/api/controller/product/ProductController.java
+++ src/main/java/com/martin/ecommerce/springecommerce/api/controller/product/ProductController.java
@@ -3,6 +3,7 @@ package com.martin.ecommerce.springecommerce.api.controller.product;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
+    @CrossOrigin("http://localhost:3000")
     public List<Product> getProducts(){
         return productService.getProducts();
     }
-
-
-    
 }
